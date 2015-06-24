9abf2667042c413091f2ef714e443add6a86dd67
Stian Haga <hagastian@gmail.com>
2015-06-24 12:08:03 +0200

    initial commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+$( document ).ready(function() {
+	function update() {
+		var now = moment();
+		var isWorking = true;
+		var day = now.day();
+		var week = now.week();
+		if(day == 1 || day == 4 || (day == 3 && week % 2 == 0)) isWorking = false;
+			$('#clock').html(isWorking ? 'Ja :D' : 'Nei :(');
+	}
+	update();
+});
