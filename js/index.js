7324027e2021d37fe771dc55b79a1936ad264cff
Stian Haga <hagastian@gmail.com>
2015-06-25 09:08:01 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,6 +8,6 @@ $( document ).ready(function() {
 		if(day == 1 || day == 4 || (day == 3 && week % 2 == 0)) isWorking = false;
 			$('#clock').html(isWorking ? 'Ja :D' : 'Nei :(');
 	}
-	update();
+	
 	setInterval(update, 60000);
 });
